ec1de2b06adf49ab01a388cd2e5489bcd9407284
Oleg Koretsky <test.radiokot@gmail.com>
2018-09-24 18:02:11 +0300

    Add Dagger modules for accounts repository

--- app/src/main/java/org/tokend/authenticator/App.kt
+++ app/src/main/java/org/tokend/authenticator/App.kt
@@ -1,9 +1,22 @@
 package org.tokend.authenticator
 
 import android.support.multidex.MultiDexApplication
+import org.tokend.authenticator.base.logic.di.AppComponent
+import org.tokend.authenticator.base.logic.di.AppDatabaseModule
+import org.tokend.authenticator.base.logic.di.DaggerAppComponent
 
 class App : MultiDexApplication() {
+    lateinit var appComponent: AppComponent
+
     override fun onCreate() {
         super.onCreate()
+
+        appComponent = DaggerAppComponent.builder()
+                .appDatabaseModule(AppDatabaseModule(DATABASE_NAME))
+                .build()
+    }
+
+    companion object {
+        const val DATABASE_NAME = "app-db"
     }
 }
