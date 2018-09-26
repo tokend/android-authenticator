b46eb4d5a446f5a1839af38f83c44831d372850b
Aleksandr <al.ivanchukov@gmail.com>
2018-09-26 15:14:15 +0300

    add monir utilities

---
+++ app/src/main/java/org/tokend/authenticator/base/util/Navigator.kt
@@ -0,0 +1,48 @@
+package org.tokend.authenticator.base.util
+
+import android.app.Activity
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.app.ActivityOptionsCompat
+import android.view.View
+import org.jetbrains.anko.intentFor
+import org.tokend.authenticator.R
+import org.tokend.authenticator.base.activities.add_account.AddAccountActivity
+
+
+/**
+ * Performs transitions between screens.
+ * 'open-' will open related screen as a child.<p>
+ * 'to-' will open related screen and finish current.
+ */
+object Navigator {
+
+    private fun fadeOut(activity: Activity) {
+        ActivityCompat.finishAfterTransition(activity)
+        activity.overridePendingTransition(0, R.anim.activity_fade_out)
+        activity.finish()
+    }
+
+    private fun createTransitionBundle(activity: Activity,
+                                       vararg pairs: Pair<View?, String>): Bundle {
+        val sharedViews = arrayListOf<android.support.v4.util.Pair<View, String>>()
+
+        pairs.forEach {
+            val view = it.first
+            if (view != null) {
+                sharedViews.add(android.support.v4.util.Pair(view, it.second))
+            }
+        }
+
+        return if (sharedViews.isEmpty()) {
+            Bundle.EMPTY
+        } else {
+            ActivityOptionsCompat.makeSceneTransitionAnimation(activity,
+                    *sharedViews.toTypedArray()).toBundle() ?: Bundle.EMPTY
+        }
+    }
+
+    fun openAddAccount(activity: Activity) {
+        activity.startActivity(activity.intentFor<AddAccountActivity>())
+    }
+}
