7b00896c4f94639705b610f414f8b9ade1a88ad7
Aleksandr <al.ivanchukov@gmail.com>
2018-09-27 17:54:46 +0300

    implement AddAccount UI

--- app/src/main/java/org/tokend/authenticator/base/activities/add_account/AddAccountActivity.kt
+++ app/src/main/java/org/tokend/authenticator/base/activities/add_account/AddAccountActivity.kt
@@ -4,15 +4,11 @@ import android.Manifest
 import android.content.Intent
 import android.os.Bundle
 import android.text.Editable
-import android.util.Log
-import com.google.gson.Gson
-import com.google.gson.JsonArray
 import io.reactivex.rxkotlin.subscribeBy
 import kotlinx.android.synthetic.main.activity_add_account.*
 import kotlinx.android.synthetic.main.layout_network_field.*
 import kotlinx.android.synthetic.main.layout_progress.*
 import okhttp3.HttpUrl
-import org.json.JSONArray
 import org.json.JSONObject
 import org.tokend.authenticator.R
 import org.tokend.authenticator.accounts.logic.CreateAccountUseCase
@@ -21,15 +17,17 @@ import org.tokend.authenticator.base.extensions.addSlashIfNeed
 import org.tokend.authenticator.base.extensions.hasError
 import org.tokend.authenticator.base.extensions.setErrorAndFocus
 import org.tokend.authenticator.base.logic.encryption.TmpEncryptionKeyProvider
-import org.tokend.authenticator.base.util.ObservableTransformers
-import org.tokend.authenticator.base.util.Permission
-import org.tokend.authenticator.base.util.QrScannerUtil
-import org.tokend.authenticator.base.util.SoftInputUtil
+import org.tokend.authenticator.base.util.*
 import org.tokend.authenticator.base.util.validators.EmailValidator
 import org.tokend.authenticator.base.view.util.LoadingIndicatorManager
 import org.tokend.authenticator.base.view.util.SimpleTextWatcher
+import org.tokend.sdk.federation.EmailAlreadyTakenException
 
 class AddAccountActivity : BaseActivity() {
+
+    companion object {
+        private val SAVE_SEED_REQUEST = "save_recovery_seed".hashCode() and 0xffff
+    }
 
     private lateinit var networkUrl: String
     private val cameraPermission = Permission(Manifest.permission.CAMERA, 404)
@@ -109,35 +107,44 @@ class AddAccountActivity : BaseActivity() {
     private fun addAccount() {
 
         val email = email_edit_text.text.toString()
+        val keyProvider = TmpEncryptionKeyProvider()
 
         CreateAccountUseCase(
             networkUrl,
             email,
             dataCipher,
-            TmpEncryptionKeyProvider(),
+            keyProvider,
             accountsRepository)
-            .perform()
-            .compose(ObservableTransformers.defaultSchedulersSingle())
-            .doOnSubscribe {
-                isLoading = true
-            }
-            .doOnError {
-                isLoading = false
-            }
-            .subscribeBy(
-                    onSuccess = {
-                        canAddAccount = false
-                        isLoading = false
-                        finish()
-                    },
-                    onError = {
-                        it.printStackTrace()
-                        handleAddError(it)
-                    })
+                .perform()
+                .compose(ObservableTransformers.defaultSchedulersSingle())
+                .doOnSubscribe {
+                    isLoading = true
+                }
+                .doOnError {
+                    isLoading = false
+                }
+                .subscribeBy(
+                        onSuccess = {
+                            canAddAccount = false
+                            isLoading = false
+                            Navigator.openRecoverySeedSaving(this,
+                                    SAVE_SEED_REQUEST,
+                                    it.recoverySeed.toString())
+                        },
+                        onError = {
+                            it.printStackTrace()
+                            handleAddError(it)
+                        })
     }
 
     private fun handleAddError(error: Throwable) {
-
+        when (error) {
+            is EmailAlreadyTakenException ->
+                email_edit_text.setErrorAndFocus(R.string.error_email_already_taken)
+            else ->
+                errorHandlerFactory.getDefault().handle(error)
+        }
+        updateAddAccountAvailability()
     }
 
     private fun tryOpenQrScanner() {
@@ -155,13 +162,21 @@ class AddAccountActivity : BaseActivity() {
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
-        QrScannerUtil.getStringFromResult(requestCode, resultCode, data)?.also {
-
-            val api = JSONObject(it).getString("api").addSlashIfNeed()
-            HttpUrl.parse(api).also { httpUrl ->
-                networkUrl = httpUrl.toString()
-                network_edit_text.setText(httpUrl.toString())
+        if(requestCode == SAVE_SEED_REQUEST) {
+            onSuccessfulCreated()
+        } else {
+            QrScannerUtil.getStringFromResult(requestCode, resultCode, data)?.also {
+                val api = JSONObject(it).getString("api").addSlashIfNeed()
+                HttpUrl.parse(api).also { httpUrl ->
+                    networkUrl = httpUrl.toString()
+                    network_edit_text.setText(httpUrl.host())
+                }
             }
         }
+    }
+
+    private fun onSuccessfulCreated() {
+        toastManager.long(getString(R.string.acount_created))
+        finish()
     }
 }
