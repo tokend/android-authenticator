6047ec6cdd5bdd78b41ae399fdaa1c3aad91a5e0
Oleg Koretsky <test.radiokot@gmail.com>
2018-10-01 17:01:26 +0300

    Add global Rx error handler

--- app/src/main/java/org/tokend/authenticator/App.kt
+++ app/src/main/java/org/tokend/authenticator/App.kt
@@ -1,10 +1,15 @@
 package org.tokend.authenticator
 
 import android.support.multidex.MultiDexApplication
+import android.util.Log
+import io.reactivex.exceptions.UndeliverableException
+import io.reactivex.plugins.RxJavaPlugins
 import org.tokend.authenticator.base.logic.di.AppComponent
 import org.tokend.authenticator.base.logic.di.AppDatabaseModule
 import org.tokend.authenticator.base.logic.di.AppModule
 import org.tokend.authenticator.base.logic.di.DaggerAppComponent
+import java.io.IOException
+import java.net.SocketException
 
 class App : MultiDexApplication() {
     lateinit var appComponent: AppComponent
@@ -16,6 +21,38 @@ class App : MultiDexApplication() {
                 .appModule(AppModule(this))
                 .appDatabaseModule(AppDatabaseModule(DATABASE_NAME))
                 .build()
+
+        initRxErrorHandler()
+    }
+
+    private fun initRxErrorHandler() {
+        RxJavaPlugins.setErrorHandler {
+            var e = it
+            if (e is UndeliverableException) {
+                e = e.cause
+            }
+            if ((e is IOException) || (e is SocketException)) {
+                // fine, irrelevant network problem or API that throws on cancellation
+                return@setErrorHandler
+            }
+            if (e is InterruptedException) {
+                // fine, some blocking code was interrupted by a dispose call
+                return@setErrorHandler
+            }
+            if ((e is NullPointerException) || (e is IllegalArgumentException)) {
+                // that's likely a bug in the application
+                Thread.currentThread().uncaughtExceptionHandler
+                        .uncaughtException(Thread.currentThread(), e)
+                return@setErrorHandler
+            }
+            if (e is IllegalStateException) {
+                // that's a bug in RxJava or in a custom operator
+                Thread.currentThread().uncaughtExceptionHandler
+                        .uncaughtException(Thread.currentThread(), e)
+                return@setErrorHandler
+            }
+            Log.w("RxErrorHandler", "Undeliverable exception received", e)
+        }
     }
 
     companion object {
