f2dfaacc41baa8a096ba4a7e17835a6a6c365b3a
Oleg Koretsky <test.radiokot@gmail.com>
2018-12-07 16:52:04 +0200

    Increment DB version to 4

--- app/src/main/java/org/tokend/authenticator/base/logic/db/AppDatabase.kt
+++ app/src/main/java/org/tokend/authenticator/base/logic/db/AppDatabase.kt
@@ -12,7 +12,7 @@ import org.tokend.authenticator.signers.storage.SignersDao
             AccountEntity::class,
             SignerEntity::class
         ],
-        version = 3,
+        version = 4,
         exportSchema = false
 )
 abstract class AppDatabase : RoomDatabase() {
