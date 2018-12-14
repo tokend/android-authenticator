1dfa36f91d6638914ec3b26220cb1a0198cd354a
Aleksandr <al.ivanchukov@gmail.com>
2018-12-14 16:16:11 +0200

    Updated account color generator algorithm

--- app/src/main/java/org/tokend/authenticator/accounts/add/view/util/AccountLogoFactory.kt
+++ app/src/main/java/org/tokend/authenticator/accounts/add/view/util/AccountLogoFactory.kt
@@ -14,14 +14,25 @@ import org.tokend.authenticator.accounts.data.model.Account
  */
 class AccountLogoFactory(private val context: Context) {
     private val colors = listOf(
-            "#EF9A9A", "#F48FB1", "#CE93D8",
-            "#FF8A80", "#FF80AB", "#90CAF9",
-            "#B39DDB", "#9FA8DA", "#8C9EFF",
-            "#80DEEA", "#80CBC4", "#82B1FF",
-            "#A5D6A7", "#C5E1A5", "#DCE775",
-            "#FBC02D", "#FFD54F", "#FFCC80",
-            "#FFAB91", "#BCAAA4", "#BDBDBD",
-            "#B0BEC5"
+            "#80CBC4",
+            "#80DEEA",
+            "#82B1FF",
+            "#8C9EFF",
+            "#90CAF9",
+            "#9FA8DA",
+            "#A5D6A7",
+            "#B0BEC5",
+            "#BCAAA4",
+            "#C5E1A5",
+            "#CE93D8",
+            "#DCE775",
+            "#EF9A9A",
+            "#F48FB1",
+            "#FBC02D",
+            "#FF8A80",
+            "#FFAB91",
+            "#FFCC80",
+            "#FFD54F"
     )
             .map { Color.parseColor(it) }
 
@@ -30,25 +41,27 @@ class AccountLogoFactory(private val context: Context) {
                               @ColorInt
                               fontColor: Int = ContextCompat.getColor(context, R.color.white)
     ): Bitmap {
-        val code = "${account.email}${account.network.name}".hashCode() and 0xffff
-
-        val background = colors[code % colors.size]
-        return get(account.network.name, size, background, fontColor)
+        val string = "${account.network.name}${account.email}"
+        val code = string.toCharArray().fold(0) { result, char ->
+            (31 * result + char.toInt()) % colors.size
+        }
+        val background = colors[code]
+        return getForValue(account.network.name, size, background, fontColor)
     }
 
     /**
      * Returns [Bitmap] logo for given asset code by first letter.
      * If first letter cannot be displayed it will be replaced with emoj.
      */
-    fun get(networkName: String,
-            size: Int,
-            @ColorInt
-            backgroundColor: Int = ContextCompat.getColor(context, R.color.colorAccent),
-            @ColorInt
-            fontColor: Int = ContextCompat.getColor(context, R.color.white)
+    fun getForValue(networkName: String,
+                    size: Int,
+                    @ColorInt
+                    backgroundColor: Int,
+                    @ColorInt
+                    fontColor: Int
     ): Bitmap {
         val letter = networkName.firstOrNull()?.toString()
-        val key = "${letter}_${size}_$backgroundColor"
+        val key = "${networkName}_$size"
         val cached = cache.get(key)
         return cached
                 ?: generate(letter, size, backgroundColor, fontColor).also { cache.put(key, it) }
@@ -72,7 +85,7 @@ class AccountLogoFactory(private val context: Context) {
 
         paint.color = fontColor
         paint.style = Paint.Style.FILL
-        paint.textSize = size * 0.45f
+        paint.textSize = size * 0.65f
         paint.textAlign = Paint.Align.CENTER
         paint.typeface = Typeface.SANS_SERIF
 
